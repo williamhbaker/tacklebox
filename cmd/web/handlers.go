2eda101e89ae77c3bc67a4b0bed365246c626f50
William Baker <wbaker@gmail.com>
2020-11-24 14:32:16 -0500

    Parsing JSON post body

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -9,6 +11,23 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) postHook(w http.ResponseWriter, r *http.Request) {
+
+	contentType := r.Header.Get("Content-Type")
+
+	if contentType != "application/json" {
+		msg := "Content-Type header is not application/json"
+		http.Error(w, msg, http.StatusUnsupportedMediaType)
+		return
+	}
+
+	var rb interface{}
+	err := json.NewDecoder(r.Body).Decode(&rb)
+	if err != nil {
+		app.errorLog.Println(err)
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
 	binID := r.URL.Query().Get(":binID")
 	if binID == "" {
 		w.WriteHeader(http.StatusNotFound)
@@ -16,6 +35,7 @@ func (app *application) postHook(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("%v", rb)))
 }
 
 func (app *application) getHooks(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -3,13 +3,7 @@ module github.com/wbaker85/tacklebox
 go 1.15
 
 require (
-	github.com/0xAX/notificator v0.0.0-20191016112426-3962a5ea8da1 // indirect
 	github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40
-	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
-	github.com/codegangsta/gin v0.0.0-20171026143024-cafe2ce98974 // indirect
-	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/justinas/alice v1.2.0
-	github.com/mattn/go-shellwords v1.0.10 // indirect
 	github.com/namsral/flag v1.7.4-pre
-	gopkg.in/urfave/cli.v1 v1.20.0 // indirect
 )
