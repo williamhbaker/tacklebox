8979183d7dd4dbd0097814a1fbc5e51320a079e7
William Baker <wbaker@gmail.com>
2020-11-28 10:18:41 -0500

    Working on requests

--- ui/src/api/index.js
+++ ui/src/api/index.js
@@ -1,15 +1,28 @@
+import axios from 'axios';
+
 const URL = process.env.REACT_APP_ENDPOINT;
 
 export const login = async (data) => {
-  let json = JSON.stringify(data);
+  // let json = JSON.stringify(data);
 
-  let init = {
-    method: 'POST',
+  // let init = {
+  //   method: 'POST',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   credentials: 'include',
+  //   body: json,
+  // };
+
+  // let response = await fetch(`${URL}/login`, init);
+
+  // let response = await axios.get(`${URL}`);
+
+  let response = await axios({
+    method: 'post',
+    url: `${URL}/login`,
     headers: { 'Content-Type': 'application/json' },
-    body: json,
-  };
-
-  let response = await fetch(`${URL}/login`, init);
+    // withCredentials: true,
+    data,
+  });
 
   console.log(response);
 };
